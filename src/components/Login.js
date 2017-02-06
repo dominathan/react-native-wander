f2a20e304b86a3c0715899c986aa9d86598ecf94
Nathan Hall <nathan.mh@gmail.com>
2017-02-05 23:40:48 -0500

    add facebook users

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 import Auth0Lock from 'react-native-lock';
 import { AUTH0_CLIENT_ID, AUTH0_DOMAIN } from '../../config/auth0';
-
+import { API_BASE } from '../../config/apiBase';
 
 const lock = new Auth0Lock({
   clientId: AUTH0_CLIENT_ID,
@@ -24,10 +24,38 @@ export class Login extends Component {
           console.log(err);
           return;
         }
-        console.log('COMING IN: ', JSON.stringify(profile));
-        console.log('TOKEN: ', JSON.stringify(token));
+        this.handleLoginSuccess(profile, token);
       })
     );
+  }
+
+  handleLoginSuccess(profile, token) {
+    //send profile to API and verify token
+    fetch(`${API_BASE}/users`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Origin': '',
+        'Authorization': `Bearer ${token.idToken}`
+      },
+      body: JSON.stringify({
+        user: this.parseProfile(profile)
+      })
+    })
+    .catch((err) => {
+      console.log('FUCK BALLS', err);
+    });
+  }
+
+  parseProfile(profile) {
+    return {
+      first_name: profile.extraInfo.given_name,
+      last_name: profile.extraInfo.family_name,
+      birthday: profile.extraInfo.birthday,
+      photo_url: profile.extraInfo.picture_large,
+      email: profile.email,
+    };
   }
 
   render() {
